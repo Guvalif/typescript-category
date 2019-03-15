42442adc401aaafbd4b356af366a5385cc1d361f
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2019-03-15 16:58:57 +0900

    Add natural transformation sample

---
+++ example-09.ts
@@ -0,0 +1,22 @@
+/**
+ * @file Example 09
+ * @author Kazuyuki TAKASE <takase@plen.jp>
+ * @copyright PLEN Project Company Inc, and all authors
+ * @license The MIT License (See also : http://opensource.org/licenses/mit-license.php)
+ */
+
+const f = (x: number): string  => x.toString();
+const g = (y: string): boolean => y.length % 2 === 0;
+
+const length_A = (xs: Array<number>): number  => xs.length; // 自然変換を定める射
+const length_B = (ys: Array<string>): number  => ys.length; // 自然変換を定める射
+const length_C = (zs: Array<boolean>): number => zs.length; // 自然変換を定める射
+
+const id_n = (x: number): number => x;
+
+const xs = [ 0, 1, 2, 3, 4 ];
+const ys = xs.map(f);
+const zs = xs.map(f).map(g);
+
+console.log( id_n(length_A(xs)) === length_B(ys) );
+console.log( id_n(length_B(ys)) === length_C(zs) );
